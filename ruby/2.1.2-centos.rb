0ff34fb90c4f3fd454f5b3f2e227ef0d0b90e489
Javier Juarez <javier.juarez@gmail.com>
2014-07-14 12:40:03 +0200

    Soporte para las nuevas MRIs

---
+++ ruby/2.1.2-centos.rb
@@ -0,0 +1,25 @@
+class Ruby210 < FPM::Cookery::Recipe
+  description 'The Ruby virtual machine'
+
+  name     'ruby'
+  version  '1:2.1.2'
+  revision 0
+  homepage 'http://www.ruby-lang.org/'
+  source   'http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.bz2'
+  sha256   '6948b02570cdfb89a8313675d4aa665405900e27423db408401473f30fc6e901'
+
+  section       'interpreters'
+  build_depends 'autoconf', 'readline-dev', 'bison', 'zlib-dev', 'openssl-dev', 'libyaml-dev'
+  depends       'ffi', 'ncurses', 'readline', 'openssl', 'tinfo5', 'libyaml', 'zlib'
+
+  def build
+
+    configure :prefix => prefix, 'disable-install-doc' => true
+    make
+  end
+
+  def install
+
+    make :install, 'DESTDIR' => destdir
+  end
+end
