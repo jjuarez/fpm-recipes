d057711c60b59962eb01a7b4b8fab32b7ac7452f
Javier Juarez <jjuarez@tuenti.com>
2016-02-05 01:10:50 +0100

    Azkaban solo recipe

---
+++ azkaban-solo/azkaban-solo.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+##
+# Default config values
+declare readonly DEFAULT_AZKABAN_HOME="/opt/azkaban-solo"
+declare readonly DEFAULT_AZKABAN_OPTS="-Xmx2G"
+declare readonly DEFAULT_AZKABAN_TMP="/tmp"
+declare readonly DEFAULT_AZKABAN_EXECUTOR_PORT="12321"
+
+
+##
+# Setting config values, or its defaults
+AZKABAN_HOME=${AZKABAN_HOME:-${DEFAULT_AZKABAN_HOME}}
+AZKABAN_OPTS=${AZKABAN_OPTS:-${DEFAULT_AZKABAN_OPTS}}
+AZKABAN_TMP=${AZKABAN_TMP:-${DEFAULT_AZKABAN_TMP}}
+AZKABAN_EXECUTOR_PORT=${AZKABAN_EXECUTOR_PORT:-${DEFAULT_AZKABAN_EXECUTOR_PORT}}
+
+##
+# Global variables
+AZKABAN_PROCESS_PID=""
+
+
+##
+# Console
+_console( ) {
+
+  local message="${1}"
+
+  [[ -z "${message}" ]] && echo -e "${message}"
+}
+
+
+##
+# Exit from the script
+_die( ) {
+
+  local message="${1:-''}"
+  local error_code=${2:-0}
+
+  _console "${message}"
+  exit ${error_code}
+}
+
+
+##
+# Gets the PID of the process
+_get_process_pid( ) {
+  
+  AZKABAN_PROCESS_PID=$(ps -ef | grep -v 'grep' | grep '__AZKABAN_SOLO__' | awk '{ print $2 }')
+}
+
+##
+# Checks if the process in running
+_check_process( ) {
+
+  _get_process_pid
+
+  [[ -n "${AZKABAN_PROCESS_PID}" ]] && kill -0 "${AZKABAN_PROCESS_PID}" &>/dev/null || return 1
+}
+
+
+##
+# Start the process
+_do_start_server( ) {
+
+  CLASSPATH=""
+
+  for file in ${AZKABAN_HOME}/{lib,extlib,plugins}/*.jar; do
+    CLASSPATH=${CLASSPATH}:${file}
+  done 2>/dev/null
+
+  AZKABAN_OPTS="${AZKABAN_OPTS} -D__AZKABAN_SOLO__ -Dlog4j.configuration=file:/etc/azkaban-solo/log4j.properties -server -Dcom.sun.management.jmxremote -Djava.io.tmpdir=${AZKABAN_TMP} -Dexecutorport=${AZKABAN_EXECUTOR_PORT} -Dserverpath=${AZKABAN_HOME}"
+
+  ${JAVA_HOME}/bin/java ${AZKABAN_OPTS} -classpath ${CLASSPATH} azkaban.webapp.AzkabanSingleServer -conf /etc/azkaban-solo/conf ${@}
+}
+
+
+##
+# Stop the process
+_do_stop_server( ) {
+
+  _get_process_pid
+
+  [[ -n "${AZKABAN_PROCESS_PID}" ]] || _die "I can't find the process" 1
+
+  _check_process
+  
+  [[ ${?} -eq 0 ]] && kill -TERM "${AZKABAN_PROCESS_PID}" || _die "I can't found any process with PID: ${AZKABAN_PROCESS_PID}" 2
+}
+
+
+##
+# Minimal requirements
+[[ -n "${AZKABAN_HOME}" ]] || _die "AZKABAN_HOME evironment variable is not set" 1
+[[ -n "${JAVA_HOME}"    ]] || _die "JAVA_HOME evironment variable is not set" 1
+
+
+##
+# ::main::
+case ${1} in
+  start|stop) 
+    _do_${1}_server
+    ;;
+
+  restart) 
+    _do_stop_server &&
+    _do_start_server
+    ;;
+
+  *) 
+    _die "WTF! I don't know what hell is '${1}'" 1
+    ;;
+esac
+
---
+++ azkaban-solo/recipe.rb
@@ -0,0 +1,47 @@
+##
+# Azkaban Solo server from LinkedIn Corp.
+class AzkabanSolo < FPM::Cookery::Recipe
+
+  description 'The most simple form of Azkaban server'
+  name     'azkaban-solo'
+  version  '2.5.0'
+  revision 0
+  license  'Apache License, Version 2.0'
+  homepage 'http://azkaban.github.io/'
+  source   "https://s3.amazonaws.com/azkaban2/azkaban2/#{version}/azkaban-solo-server-#{version}.tar.gz"
+  md5      'a3a5a681f040cd4f7d4032e40edb0953'
+  arch     'all'
+  section  'databases'
+  depends 'default-jre-headless'
+
+  post_install   'post-install'
+  post_uninstall 'post-uninstall'
+
+  config_files %w{
+    /etc/default/azkaban-solo
+    /etc/azkaban-solo/azkaban-users.xml
+    /etc/azkaban-solo/log4j.properties
+    /etc/azkaban-solo/global.properties
+    /etc/azkaban-solo/azkaban.properties
+    /etc/azkaban-solo/azkaban.private.properties
+  }
+
+  def build
+    puts "build"
+  end
+
+
+  def install
+
+		opt('azkaban-solo').install Dir['*']
+		opt('azkaban-solo/bin').install workdir('azkaban-solo.sh')
+
+		etc('default').install workdir('azkaban-solo.default'), 'azkaban-solo'
+		etc('init.d').install workdir('azkaban-solo.init'), 'azkaban-solo'
+		etc('security/limits.d').install workdir('azkaban-solo.limits'), 'azkaban-solo.conf'
+		etc('logrotate.d').install workdir('azkaban-solo.logrotate'), 'azkaban-solo.conf'
+
+    etc('azkaban-solo').install Dir['config/*']
+  end
+end
+
